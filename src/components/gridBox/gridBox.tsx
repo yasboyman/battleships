b10fa5f79809dae8e4163652e1e01d2caa00f89b
Yasir Alabadi <yasir@MMT-MAC-4396.local>
2023-01-08 11:47:21 +0000

    project init, using vite

---
+++ src/components/gridBox/gridBox.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GridBox = ({number}: any) => {
+    return (
+        <div style={{ width: '20px', height:'20px'}}>
+            {number}
+        </div>
+    );
+};
+
+export default GridBox;
