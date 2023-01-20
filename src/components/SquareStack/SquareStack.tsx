16f401f4022ff25d48a48747913ae5f75f0c8e95
Yasir Alabadi <yasir@MMT-MAC-4396.local>
2023-01-20 17:28:55 +0000

    animation added, commented out for the time being

--- src/components/SquareStack/SquareStack.tsx
+++ src/components/SquareStack/SquareStack.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, useContext } from "react";
+import React, { Dispatch, useContext, useState } from "react";
 import styles from "./squareStack.module.css";
 import { ActionType, StateType } from "../types";
 import { GameContext } from "../context";
@@ -8,20 +8,24 @@ interface Props {
 }
 
 const SquareStack: React.FC<Props> = ({ count }) => {
+  const [active, setActive] = useState(false);
   const [state, dispatch] =
     useContext<[StateType, Dispatch<ActionType>]>(GameContext);
 
+  const handleSquareClick = () => {
+    dispatch({ type: "SET_ACTIVE_SHIP", payload: count });
+  };
   let boxes = [];
   for (let i = 0; i < count; i++) {
     boxes.push(
-      <div
-        key={i}
-        className={styles.Box}
-        onClick={() => dispatch({ type: "SET_ACTIVE_SHIP", payload: count })}
-      />
+      <div key={i} className={styles.Box} onClick={handleSquareClick} />
     );
   }
-  return <div>{boxes}</div>;
+  return (
+    <div className={styles.boxContainer} >
+      {boxes}
+    </div>
+  );
 };
 
 export default SquareStack;
